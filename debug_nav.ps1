aa81441a545367b5373148c33478e94e8b6b68cf
psndays14 <ahmedyassinefliyou2001@gmail.com>
2026-06-10 02:27:54 +0100

    Delete debug_nav.ps1

--- debug_nav.ps1
+++
@@ -1,6 +0,0 @@
-$path = 'C:\Users\PC\Downloads\AHMED YASSINE FLIYOU-20260322T071101Z-1-001\github-ready\index.html'
-$bytes = [System.IO.File]::ReadAllBytes($path)
-$content = [System.Text.Encoding]::UTF8.GetString($bytes)
-$start = $content.IndexOf('<div class="nav-links">')
-$chunk = $content.Substring($start, 800)
-Write-Host $chunk
