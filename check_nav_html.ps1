edb91f054ab0cc059d935f011803b991268216f4
psndays14 <ahmedyassinefliyou2001@gmail.com>
2026-06-10 02:27:07 +0100

    Delete check_nav_html.ps1

--- check_nav_html.ps1
+++
@@ -1,9 +0,0 @@
-$path = 'C:\Users\PC\Downloads\AHMED YASSINE FLIYOU-20260322T071101Z-1-001\github-ready\index.html'
-$bytes = [System.IO.File]::ReadAllBytes($path)
-$content = [System.Text.Encoding]::UTF8.GetString($bytes)
-$navOpen = '<div class="nav-links">'
-$start = $content.IndexOf($navOpen)
-$btnIdx = $content.IndexOf('<button class="menu-btn"', $start)
-$chunk = $content.Substring($start, $btnIdx - $start)
-Write-Host "=== index.html nav-links ==="
-Write-Host $chunk
